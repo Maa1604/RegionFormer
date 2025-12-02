f83164ca6adf44fe0aed1a4d9f82bfc1ed24bee8
whoami <mohamedaasalas@gmail.com>
2025-12-02 14:21:30 +0100

    Cambio en sh

--- run_experiment_percentage.sh
+++ run_experiment_percentage.sh
@@ -27,7 +27,145 @@ python train_one_epoch_and_validate_region.py \
 
 
 # Run WITH mask
-OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask/
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_5/
+
+
+echo "Running experiment WITH mask..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --use_mask \
+  --percent 5
+
+
+# Run WITH mask only
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_only_5/
+
+
+echo "Running experiment WITH mask only..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --mask_only \
+  --percent 5
+
+
+# Run WITH mask
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_10/
+
+
+echo "Running experiment WITH mask..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --use_mask \
+  --percent 10
+
+
+# Run WITH mask only
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_only_10/
+
+
+echo "Running experiment WITH mask only..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --mask_only \
+  --percent 10
+
+
+# Run WITH mask
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_20/
+
+
+echo "Running experiment WITH mask..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --use_mask \
+  --percent 20
+
+
+# Run WITH mask only
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_only_20/
+
+
+echo "Running experiment WITH mask only..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --mask_only \
+  --percent 20
+
+
+# Run WITH mask
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_50/
 
 
 echo "Running experiment WITH mask..."
@@ -49,8 +187,8 @@ python train_one_epoch_and_validate_region.py \
   --percent 50
 
 
-# Run WITH mask
-OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_only/
+# Run WITH mask only
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_only_50/
 
 
 echo "Running experiment WITH mask only..."
@@ -70,3 +208,48 @@ python train_one_epoch_and_validate_region.py \
   --epochs 20 \
   --mask_only \
   --percent 50
+
+  # Run WITH mask
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_80/
+
+
+echo "Running experiment WITH mask..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --use_mask \
+  --percent 80
+
+
+# Run WITH mask only
+OUT_DIR=./EXPERIMENTS/padchestgr_train_tf_prefix_jpg_region_15_with_mask_only_80/
+
+
+echo "Running experiment WITH mask only..."
+python train_one_epoch_and_validate_region.py \
+  --train_pkl "$TRAIN_PKL" \
+  --val_pkl "$VAL_PKL" \
+  --out_dir "$OUT_DIR" \
+  --prefix "$PREFIX" \
+  --only_prefix \
+  --mapping_type transformer \
+  --num_layers 8 \
+  --prefix_length 40 \
+  --prefix_length_clip 40 \
+  --bs 40 \
+  --beam \
+  --gen_len 15 \
+  --epochs 20 \
+  --mask_only \
+  --percent 80
